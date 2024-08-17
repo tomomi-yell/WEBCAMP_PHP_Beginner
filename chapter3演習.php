78f278326eaac8d20fa280c0f5cd0442e08316ae
TOMOMI <tomomi@example.com>
2024-08-17 04:29:07 +0000

    chapter3演習.phpを書いた

---
+++ chapter3演習.php
@@ -0,0 +1,17 @@
+<?php
+
+// 商品価格の代入
+$price = 1800;
+
+// 税込み価格の計算
+$price_with_tax = $price * 1.10;
+
+// 表示
+echo "{$price_with_tax} \n";
+
+//姓と名の代入
+$family_name = "Tamura";
+$first_name = "Tomomi";
+
+//結合して出力
+echo "{$family_name} {$first_name} \n";
