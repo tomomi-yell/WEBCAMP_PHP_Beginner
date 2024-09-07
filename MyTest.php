6371d4034872cc04381b065136d1cecaa9c5b838
TOMOMI <tomomi@example.com>
2024-09-07 02:52:45 +0000

    MyTest.phpを書いた

---
+++ MyTest.php
@@ -0,0 +1,16 @@
+<?php
+//
+class MyTest
+{
+    //
+    public function add($i, $j)
+    {
+        return $i + $j;
+    }
+
+    //
+    public function sub($i, $j)
+    {
+        return $i - $j;
+    }
+}
